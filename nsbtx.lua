514e265557c2871f97e792795123495a1dcb1ade
Astral-C <astral-c@users.noreply.github.com>
2025-05-23 08:34:38 -0400

    remove logging thats not needed

--- nsbtx.lua
+++ nsbtx.lua
@@ -1,8 +1,6 @@
 local function readDict(file, readFunc, args)
-    local log = io.open("log_dict.txt", "w")
     file:seek("cur", 1)
     local size = string.unpack("<b", file:read(1))
-    log:write("Reported Dict Size is ", size, "\n")
 
     local values = {}
     local items = {}
@@ -15,7 +13,6 @@ local function readDict(file, readFunc, args)
 
     for i = 1, size, 1 do
         local name = string.unpack("<c16", file:read(16)):gsub("%z+$", ""):gsub("%z+", "")
-        log:write("Name is ", name, "\n")
         items[i] = { name, values[i] }
     end
 
@@ -32,12 +29,7 @@ local function readPalette(file, args)
     local paletteOffset = (string.unpack("<H", file:read(2)) << 3) + args[1]
     local listPos = file:seek("cur")
 
-    local log = io.open("log_read_pal.txt", "w")
-
     local colorCount = args[2] / 2
-    log:write("# of Colors ")
-    log:write(colorCount)
-    log:write("\nPalette Offset: ", paletteOffset, "\n")
 
     file:seek("set", paletteOffset)
     palette:resize(colorCount)
@@ -104,7 +96,6 @@ end
 local function import(plugin)
     local dlg = Dialog("Import NSBTX")
 
-
     dlg:file {
         id = "source",
         label = "Source:",
@@ -126,16 +117,12 @@ local function import(plugin)
                 return
             end
 
-            local log = io.open("log_main.txt", "w")
-
             local file <close> = io.open(source, "rb")
             if not file then
-                log:write("Failed to open file?")
                 return
             end
 
             local magic = string.unpack("<c4", file:read(4))
-            log:write("BTX0 = ", magic, "\n")
             if "BTX0" ~= magic then
                 return
             end
@@ -145,12 +132,10 @@ local function import(plugin)
             local sectionCount = string.unpack("<H", file:read(2))
 
             for i = 1, sectionCount, 1 do
-                log:write("Reading Section ", i, "\n")
                 local sectionOffset = string.unpack("<I4", file:read(4))
                 local returnOffset = file:seek("cur")
 
                 local stamp = string.unpack("<c4", file:read(4))
-                log:write("Stamp ", stamp, "\n")
                 if stamp == "TEX0" then
                     file:seek("cur", 8) -- skip size and unknown
                     local textureDataSize, textureListOff, _, textureDataOff, _ = string.unpack("<HHI4I4I4",
@@ -179,8 +164,8 @@ local function import(plugin)
                     local frame = 1
                     for name, value in ipairs(nxbtx_images) do
                         if dlg.data.textureascel then
-                            if #sprite.frames - 1 >= frame then
-                                sprite:newEmptyFrame(frame)
+                            if #sprite.frames - 1 <= frame then
+                                sprite:newEmptyFrame()
                             end
                             sprite:newCel(sprite.layers[1], frame, value[2]["data"])
                             frame = frame + 1
